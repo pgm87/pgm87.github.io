81f5d447c3a4b99eab3656ff58fd3a8dbcd86671
pgm87 <38256944+pgm87@users.noreply.github.com>
2022-08-10 14:44:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,88 @@
-$(document).ready(function () {
 
-    // List of Klngon Words
-    const klingonDictionaryArray = [
-        "par'Mach",
-        "bang bom",
-        "boqrat",
-        "chemvaH",
-        "DIr ’In",
-        "ghab tun",
-        "gham",
-        "chelwI’",
-        "ghISnar",
-        "tI’qa’ vIghro’",
-        "bat'leth",
-        "be’taSeD",
-        "Qu’vatlh",
-        "batlh",
-        "pujwI’",
-    ];
-    // Global Declaration of numOfWords variable.
-    let numOfWords;
+const app = {};
+let subjectSearch;
+app.$bookdisplay = $('.bookDisplay');
+app.$container = $('.container');
+app.$form = $('form');
+app.$searchSubject = $('#searchSubject');
+app.$bookIcon = $('.bookIcon');
+const urlSearch = `https://tranquil-brook-97370.herokuapp.com/http://openlibrary.org/subjects/`;
 
-    // Submit event for generating klingon ipsum
-    $('form').on('submit', (event) => {
-        event.preventDefault();
-        // Gets number from the submit form and makes it equal to numOfWords
-        numOfWords = parseInt($('#numOfWords').val());
-        // If statement making sure it can't be less than 0 or greater than 500
-        if (numOfWords <= 0 || numOfWords >= 500) {
-            alert('Please input a number greater than 0 and less then 500!')
+
+
+
+
+
+// app init function
+app.init = () => {
+    app.getSubject();
+
+}
+
+// Url append function
+app.urlAppendFunction = (id, title, cover) => {
+    if (cover === null) {
+        app.$bookdisplay.append(`<div class="container">
+    <a href="https://openlibrary.org${id}"><p>${title}</p>  <div class="imageContainer"><img src="./assets/No_image_available.svg"></div> </a></div>`);
+    }
+    else {
+        app.$bookdisplay.append(`<div class="container">
+        <a href="https://openlibrary.org${id}"><p>${title}</p> <div class="imageContainer"> <img src="https://covers.openlibrary.org/b/id/${cover}-M.jpg" alt="Picture of book cover"></div></a></div>`);
+    }
+}
+
+//  Get Subject from form, and toggle animation
+app.getSubject = () => {
+    app.$form.on('submit', (e) => {
+        e.preventDefault();
+        let subjectSearch = app.$searchSubject.val();
+        app.$bookIcon.toggleClass('bookRotate')
+        app.$bookIcon.removeClass('bookHide');
+        app.searchSubject(subjectSearch);
+        app.emptyDisplay();
+    })
+}
+
+// Check to see if results are good
+app.checkResults = (result) => {
+    setTimeout(function () {
+        if (result.works.length == 0) {
+            alert('Nothing was Found, check your spelling or try something less specific')
         }
-        // Loop which runs through the array selecting random words up to the numOfWords submitted
         else {
-            for (i = 0; i < numOfWords; i++) {
-                $('p').append(` ${klingonDictionaryArray[getRandomIndex()]}`);
-                // Disables button
-                $("#btnSubmit").attr("disabled", true)
-
-            }
+            app.$bookIcon.toggleClass('bookHide')
+            app.displayBooks(result);
         }
+    }, 3000)
+}
 
+
+// search the subjects
+app.searchSubject = (subject) => {
+    const searchResults = $.ajax({
+        url: `${urlSearch}${subject}.json?details=true`,
+        method: "GET",
+        dataType: "json",
+    }).then(result => {
+
+        app.checkResults(result);
+
+    })
+}
+// display the subjects
+app.displayBooks = (result) => {
+    app.$bookIcon.removeClass('bookRotate');
+    result.works.forEach(book => {
+        app.urlAppendFunction(book.key, book.title, book.cover_id);
     });
+}
+// clear display
+app.emptyDisplay = () => {
+    app.$bookdisplay.empty();
+};
 
-    // Resets form
-    $('form').on('reset', () => {
-        $('p').empty();
-        $("#btnSubmit").attr("disabled", false)
 
-    });
-    // Gets random index from the length of the array.
-    const getRandomIndex = () => {
-        let randomNumber = Math.floor(Math.random() * klingonDictionaryArray.length);
-        return randomNumber;
-
-    };
-
+// start the app
+$(document).ready(function () {
+    app.init();
 });
